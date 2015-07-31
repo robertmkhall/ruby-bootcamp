5fff3402746b569b52254d4815d6e2cdce9770bc
unknown <RHA29@LEELT0116477.bskyb.com>
2015-07-31 16:36:54 +0100

    String exercises

--- exercises/strings1/string_gymnastics_1.rb
+++ exercises/strings1/string_gymnastics_1.rb
@@ -23,3 +23,13 @@ LIST
 Given the following price list and shopping list print out the total cost of 
 the shopping list in pounds and pence
 =end 
+
+
+def calcTotal(prices, shoppingList)
+  filtered = shoppingList.split.select { |item| prices.has_key?(item) }
+  filtered.inject(0.00) { |total, item| prices[item] + total } 
+  filtered.to_f
+end
+
+total = calcTotal(prices, shopping_list)
+puts "The price of the shopping list is: #{total}"
--- exercises/strings2/string_gymnastics_2.rb
+++ exercises/strings2/string_gymnastics_2.rb
@@ -1,4 +1,4 @@
-price_list = "orange = 10p apple = 20p bread = £1.10 tomato = 25p cereal = £2.34" 
+﻿price_list = "orange = 10p apple = 20p bread = £1.10 tomato = 25p cereal = £2.34" 
 
 shopping_list=<<LIST
  list
@@ -17,3 +17,24 @@ LIST
 Given the following price list and shopping list print out the total cost of 
 the shopping list in pounds and pence
 =end 
+
+def replaceInvalidChars(price)
+  decimalised = price[/[.]/] ? price : price + ".00" 
+  decimalised.gsub(/[£p]/, '').to_f
+end
+
+def splitPrices(prices) 
+  split = prices.split.select { |item| item != '='}
+  withoutInvalids = split.map { |item| item[/\d/] ? replaceInvalidChars(item) : item}
+  Hash[*withoutInvalids]  
+end
+
+def calcTotal(prices, shoppingList)
+  filtered = shoppingList.split.select { |item| prices.has_key?(item) }
+  filtered.inject(0.00) { |total, item| prices[item] + total } 
+end
+
+convertedPrices = splitPrices(price_list)
+total = calcTotal(convertedPrices, shopping_list)
+
+puts "The price of the shopping list is: £#{total}"
