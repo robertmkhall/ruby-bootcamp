dc1e52175493f869b5c016cbce005d78695f679e
RHA29 <robert.hall@sky.uk>
2015-08-04 11:32:38 +0100

    Rubyfying method names

--- exercises/strings1/string_gymnastics_1.rb
+++ exercises/strings1/string_gymnastics_1.rb
@@ -27,10 +27,10 @@ the shopping list in pounds and pence
 =end 
 
 
-def calcTotal(prices, shoppingList)
+def calc_total(prices, shoppingList)
   filtered = shoppingList.split.select { |item| prices.has_key?(item) }
   "%.2f" % filtered.inject(0.00) { |total, item| prices[item] + total } 
 end
 
-total = calcTotal(prices, shopping_list)
+total = calc_total(prices, shopping_list)
 puts "The price of the shopping list is: £#{total}"
--- exercises/strings2/string_gymnastics_2.rb
+++ exercises/strings2/string_gymnastics_2.rb
@@ -18,23 +18,23 @@ Given the following price list and shopping list print out the total cost of
 the shopping list in pounds and pence
 =end 
 
-def replaceInvalidChars(price)
+def replace_invalid_chars(price)
   decimalised = price[/[.]/] ? price : "0." + price 
   decimalised.gsub(/[£p]/, '').to_f
 end
 
-def splitPrices(prices) 
+def split_prices(prices) 
   split = prices.split.select { |item| item != '='}
-  withoutInvalids = split.map { |item| item[/\d/] ? replaceInvalidChars(item) : item}
-  Hash[*withoutInvalids]  
+  without_invalids = split.map { |item| item[/\d/] ? replace_invalid_chars(item) : item}
+  Hash[*without_invalids]  
 end
 
-def calcTotal(prices, shoppingList)
-  filtered = shoppingList.split.select { |item| prices.has_key?(item) }
+def calc_total(prices, shopping_list)
+  filtered = shopping_list.split.select { |item| prices.has_key?(item) }
   filtered.inject(0.00) { |total, item| prices[item] + total } 
 end
 
-convertedPrices = splitPrices(price_list)
-total = calcTotal(convertedPrices, shopping_list)
+converted_prices = split_prices(price_list)
+total = calc_total(converted_prices, shopping_list)
 
 puts "The price of the shopping list is: £#{total}"
