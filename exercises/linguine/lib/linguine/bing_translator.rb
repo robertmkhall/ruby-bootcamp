d5e3215239a4a75460399f8290f7c0f891b2e89c
rha29 <robert.hall@sky.uk>
2015-08-15 11:35:20 +0100

    Implemented bing translator authorisation generator

--- exercises/linguine/lib/linguine/bing_translator.rb
+++ exercises/linguine/lib/linguine/bing_translator.rb
@@ -1,6 +1,46 @@
+require 'faraday'
+require 'json'
+# require 'bing_translator'
+
 class BingTranslator
 
+  ACCESS_TOKEN_URI = "https://datamarket.accesscontrol.windows.net/"
+
   def translate(text)
-    text
+
+    conn = Faraday.new(:url => 'http://api.microsofttranslator.com/') do |faraday|
+      faraday.request :url_encoded # form-encode POST params
+    end
+
+    conn.get do |req|
+      req.url '/v2/Http.svc/Translate'
+      req.params['text'] = text
+      req.params['from'] = 'en'
+      req.params['to'] = 'de'
+    end
+  end
+
+  def access_token
+    return @access_token if @access_token and Time.now < @access_token[:expires_in]
+
+    conn = Faraday.new(:url => ACCESS_TOKEN_URI) do |faraday|
+      faraday.request :url_encoded # form-encode POST params
+      faraday.response :logger # log requests to STDOUT
+      faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
+    end
+
+    response = conn.post 'v2/OAuth2-13',
+              {:client_id => 'linguine_1000',
+               :client_secret => 'vpgnTSjxeuGnxnr1MA0oJgkG0xzovPYoIzJnFXQoZzk=',
+               :scope => 'http://api.microsofttranslator.com',
+               :grant_type => 'client_credentials'}
+
+    json_response = JSON.parse(response.env[:body])
+
+    @access_token = {token: json_response['access_token'], expires_in: json_response['expires_in']}
+    @access_token
   end
 end
+
+translator = BingTranslator.new
+translator.access_token
--- exercises/linguine/test/linguine/bing_translator_spec.rb
+++ exercises/linguine/test/linguine/bing_translator_spec.rb
@@ -1,11 +1,37 @@
-require 'rspec'
+require_relative '../../lib/linguine/bing_translator'
 
 describe BingTranslator do
 
-  describe '#translate' do
-    it 'should translate the web page' do
+  subject(:translator) {described_class.new}
 
-      true.should == false
+  describe '#translate' do
+    it 'will translate the web page' do
+      expect(subject.translate('some text')).to eql('translated text')
+    end
+  end
+
+  describe '#access_token' do
+
+    let(:token) {'some random token'}
+    let(:expires_in) {'600'}
+    let(:response) {Faraday::Response.new}
+
+    before do
+      connection = Faraday::Connection.new
+
+      env = {
+        'body': "{\"access_token\": \"#{token}\", \"expires_in\": \"#{expires_in}\"}"
+      }
+
+      allow(response).to receive(:env).and_return(env)
+      allow(Faraday).to receive(:new).and_return(connection)
+      allow(connection).to receive(:post).and_return(response)
+    end
+
+    it 'will get a new access token' do
+      access_token = {token: token, expires_in: expires_in}
+
+      expect(subject.access_token).to eql(access_token)
     end
   end
 end
