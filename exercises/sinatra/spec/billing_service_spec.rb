c7dd8dce4c21385459c04f91cb58705186e6f6b5
rha29 <robert.hall@sky.uk>
2015-09-01 14:21:14 +0100

    Replaced web mock with pact stubbing to generate contract json that can be verified by the producer service

--- exercises/sinatra/spec/billing_service_spec.rb
+++ exercises/sinatra/spec/billing_service_spec.rb
@@ -6,22 +6,43 @@ describe BillingService do
   let(:valid_username) { 'robertmkhall' }
   let(:invalid_username) { 'invalid_username' }
 
-  describe '#bill' do
-    let(:expected_bill_ids_response) { "{\"ids\": [\"100000001\", \"10000002\"] }"}
-    let(:expected_json) { "{\"name\": \"Some dude\", \"date\": \"01/01/2016\", \"amount\": 999.99}" }
+  let(:expected_bill_ids_response) { "{\"ids\":[\"10000001\",\"10000002\"]}" }
+  let(:expected_json) do
+    ROOT = Pathname.new(File.expand_path(__dir__))
+    BILL_PATH = ROOT.join("resources").join('test_bill.json').to_s
 
+    File.read(BILL_PATH)
+  end
+
+  describe '#bill', :pact => true do
     before do
-      stub_request(:get, Addressable::Template.new(subject.query_uri))
-        .to_return({body: expected_bill_ids_response})
-      stub_request(:get, Addressable::Template.new(subject.request_uri))
-        .to_return({body: expected_json})
+      bill_service_producer
+          .upon_receiving('a request for bill ids')
+          .with({
+                    method: :get,
+                    path: '/bill',
+                    query: URI::encode('username=' + valid_username)
+                })
+          .will_respond_with({
+                                 status: 200,
+                                 body: expected_bill_ids_response
+                             })
+
+      bill_service_producer
+          .upon_receiving('a request for a specific bill')
+          .with({
+                    method: :get,
+                    path: '/bill/10000002'
+                })
+          .will_respond_with({
+                                 status: 200,
+                                 body: expected_json
+                             })
     end
 
     it 'will return the bill as a hash' do
       actual_bill = subject.bill(valid_username)
-      expect(actual_bill.name).to eq('Some dude')
-      expect(actual_bill.date).to eq("01/01/2016")
-      expect(actual_bill.amount).to eq(999.99)
+      expect(actual_bill).to eq(JSON.parse(expected_json))
     end
   end
 end
--- exercises/sinatra/spec/spec_helper.rb
+++ exercises/sinatra/spec/spec_helper.rb
@@ -5,8 +5,17 @@ require 'rack/test'
 require 'capybara/rspec'
 require 'rspec'
 require 'json'
-require 'webmock/rspec'
+require 'pact'
+require 'pact/consumer/rspec'
 
 $LOAD_PATH.unshift("#{__dir__}/../lib")
 
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
+
+Pact.service_consumer 'Bill Service Consumer' do
+  has_pact_with 'Bill Service Producer' do
+    mock_service :bill_service_producer do
+      port 9393
+    end
+  end
+end
